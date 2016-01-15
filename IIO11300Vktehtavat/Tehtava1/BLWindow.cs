c975dee0ff0e1e578d20eaa633a503272e60fb1e
Mika Mähönen <K6058@student.jamk.fi>
2016-01-15 21:40:58 +0200

    Tehtävä 1 ratkaistu kokonaan

--- IIO11300Vktehtavat/Tehtava1/BLWindow.cs
+++ IIO11300Vktehtavat/Tehtava1/BLWindow.cs
@@ -1,19 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+* Copyright (C) JAMK/IT/Esa Salmikangas
+* This file is part of the IIO11300 course project.
+* Created: 15.1.2016 Modified: 15.1.2016
+* Authors: Mika Mähönen (K6058) ,Esa Salmikangas
+*/
 
 namespace Tehtava1
 {
-    public class BusinessLogicWindow
+  public class BusinessLogicWindow
+  {
+    /// <summary>
+    /// CalculateWindowArea calculates the area of a window
+    /// </summary>
+    public static double CalculateWindowArea(double width, double height, double frameWidth)
     {
-        /// <summary>
-        /// CalculatePerimeter calculates the perimeter of a window
-        /// </summary>
-        public static double CalculatePerimeter(double width, double height)
-        {
-            throw new NotImplementedException();
-        }
+      if (width <= 0 || height <= 0) return 0;
+      return (width - (2 * frameWidth)) * (height - (2 * frameWidth));
     }
+
+    /// <summary>
+    /// CalculatePerimeter calculates the perimeter of a window
+    /// </summary>
+    public static double CalculatePerimeter(double width, double height)
+    {
+      if (width <= 0 || height <= 0) return 0;
+      return width * 2 + height * 2;
+    }
+
+    /// <summary>
+    /// CalculateFrameArea calculates the area of a frame
+    /// </summary>
+    public static double CalculateFrameArea(double width, double height, double frameWidth)
+    {
+      if (width <= 0 || height <= 0) return 0;
+      return (width * frameWidth * 2) + ((height - (2 * frameWidth)) * frameWidth * 2);
+    }
+  }
 }
--- IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
@@ -2,22 +2,10 @@
 * Copyright (C) JAMK/IT/Esa Salmikangas
 * This file is part of the IIO11300 course project.
 * Created: 12.1.2016 Modified: 13.1.2016
-* Authors: Tero ,Esa Salmikangas
+* Authors: Mika Mähönen (K6058), Esa Salmikangas
 */
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Tehtava1
 {
@@ -31,23 +19,27 @@ namespace Tehtava1
       InitializeComponent();
     }
 
-        private void btnCalculate_Click(object sender, RoutedEventArgs e)
-        {
-            //TODO
-            try
-            {
-                double result;
-                result = BusinessLogicWindow.CalculatePerimeter(1, 1);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                //yield to an user that everything okay
-            }
-        }
+    private void btnCalculate_Click(object sender, RoutedEventArgs e)
+    {
+      //TODO
+      try
+      {
+        double l = Math.Abs(double.Parse(txtWidth.Text));
+        double h = Math.Abs(double.Parse(txtHeight.Text));
+        double w = Math.Abs(double.Parse(txtFrameWidth.Text));
+        txtResult1.Text = BusinessLogicWindow.CalculateWindowArea(l, h, w).ToString();
+        txtResult2.Text = BusinessLogicWindow.CalculatePerimeter(l, h).ToString();
+        txtResult3.Text = BusinessLogicWindow.CalculateFrameArea(l, h, w).ToString();
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+      finally
+      {
+        //yield to an user that everything okay
+      }
+    }
 
     private void btnClose_Click(object sender, RoutedEventArgs e)
     {
