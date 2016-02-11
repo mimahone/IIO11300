6d190ef600785c1162f48a54c693027250b9cf30
Mika Mähönen <K6058@student.jamk.fi>
2016-02-11 11:11:58 +0200

    Harjoitustehtävä XML & DataBinding in WPF

---
+++ IIO11300Vktehtavat/H4TyontekijaConsole/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace H4TyontekijatConsole
+{
+  class Program
+  {
+    static void CalculateSalarySumFromXML(string filu)
+    {
+      try
+      {
+        // Tutkitaan onko tiedosto olemassa
+        if (System.IO.File.Exists(filu))
+        {
+          // Luetaan XML-tiedosto XmlDocument-olioon
+          XmlDocument xmlDoc = new XmlDocument();
+          xmlDoc.Load(filu);
+
+          // Haetaan kaikkien vakituisten työntekijöiden palkka-elementit XPath:lla
+          XmlNodeList xnl = xmlDoc.SelectNodes("/tyontekijat/tyontekija[tyosuhde='vakituinen']/palkka");
+
+          // Loopitetaan nodelista läpi
+          int sum = 0;
+
+          for (int i = 0; i < xnl.Count; i++)
+          {
+            sum += Convert.ToInt32(xnl.Item(i).InnerText);
+          }
+
+          Console.WriteLine(string.Format("Vakituisia on {0} ja heidän palkat yhteensä {1}", xnl.Count, sum));
+        }
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+    }
+
+    static void ReadWorkersFromXML(string filu)
+    {
+      try
+      {
+        // Tutkitaan onko tiedosto olemassa
+        if (System.IO.File.Exists(filu))
+        {
+          // Luetaan XML-tiedosto XmlDocument-olioon
+          XmlDocument xmlDoc = new XmlDocument();
+          xmlDoc.Load(filu);
+
+          // Haetaan kaikki työntekija-elementit XPath:lla
+          XmlNodeList xnl = xmlDoc.SelectNodes("/tyontekijat/tyontekija");
+          XmlNode xn; // Edustaa yksittäistä noodia
+          XmlNodeList xnl2;
+
+          // Loopitetaan nodelista läpi
+          for (int i = 0; i < xnl.Count; i++)
+          {
+            // Näytetään käyttäjälle noodien sisältö
+            //Console.WriteLine(xnl.Item(i).InnerText);
+            xn = xnl.Item(i);
+            xnl2 = xn.ChildNodes;
+
+            for (int j = 0; j < xnl2.Count; j++)
+            {
+              Console.WriteLine(xnl2.Item(j).InnerText);
+            }
+
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      try
+      {
+        //ReadWorkersFromXML(@"D:\Työntekijät2016.xml");
+        CalculateSalarySumFromXML(@"D:\Työntekijät2016.xml");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+  }
+}
+
+// Console-sovelluksen testaus Ctrl+F5
---
+++ IIO11300Vktehtavat/H4TyontekijatWpf/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace H4TyontekijatWpf
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    XElement xe;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void btnReadXML_Click(object sender, RoutedEventArgs e)
+    {
+      // Luetaan XML-tiedostosta tyontekija-elementit ja sidotaan ne DataGridiin
+      try
+      {
+        string filu = @"D:\Työntekijät2016.xml";
+        xe = XElement.Load(filu);
+        dgData.DataContext = xe.Elements("tyontekija");
+        tbMessage.Text = string.Format("Työntekijöitä {0} ja palkat yhteensä {1}", countWorkers("vakituinen"), calculateSalarySum("vakituinen"));
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+    }
+
+    private int countWorkers(string tyosuhde)
+    {
+      try
+      {
+        var temp = from ele in xe.Elements()
+                   where ele.Element("tyosuhde").Value == tyosuhde
+                   select ele.Elements("sukunimi");
+        return temp.Count();        
+        //return xe.Elements("tyontekija").Count();
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+    }
+
+    private decimal calculateSalarySum(string tyosuhde)
+    {
+      try
+      {
+        var temp = from ele in xe.Elements()
+                   where ele.Element("tyosuhde").Value == tyosuhde
+                   select ele.Element("palkka");
+        return temp.Sum(n => decimal.Parse(n.Value));
+        //return xe.Elements("tyontekija").Elements("palkka").Sum(n => decimal.Parse(n.Value));
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+    }
+
+  }
+}
