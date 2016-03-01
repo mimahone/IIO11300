dfe06f77d492b0f1bc51d3bc2d852dae7995fed3
Mika Mähönen <K6058@student.jamk.fi>
2016-03-01 17:39:28 +0200

    Viikkotehtävä #07: Salasanan vahvuuden tarkistaja aloitettu (UI Bindaukset ei vielä toimi!)

---
+++ IIO11300Vktehtavat/Salasanavahvuus/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Salasanavahvuus
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    PasswordStrength ps = new PasswordStrength();
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      spPasswordStrength.DataContext = ps;
+      pwdPassword.Focus();
+    }
+
+    public PasswordStrength PasswordChecker { get { return ps; } }
+
+    private void pwdPassword_KeyUp(object sender, KeyEventArgs e)
+    {
+      ps.Password = pwdPassword.Password;
+    }
+  }
+}
---
+++ IIO11300Vktehtavat/Salasanavahvuus/PasswordStrength.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+
+namespace Salasanavahvuus
+{
+  public class PasswordStrength
+  {
+
+    public PasswordStrength(string password = "")
+    {
+      Password = password;
+    }
+
+    public string Password { get; set; }
+
+    public int Chars { get { return Password.Length; } }
+
+    public int UpperCaseLetters { get { return Password.Count(c => char.IsUpper(c)); } }
+
+    public int LowerCaseLetters { get { return Password.Count(c => char.IsLower(c)); } }
+
+    public int Numbers { get { return Password.Count(c => char.IsNumber(c)); } }
+
+    public int SpecialMarks { get { return Password.Count(c => !char.IsLetterOrDigit(c)); } }
+
+    public int MarkTypes {
+      get { return countMarkTypes(); }
+    }
+
+    public string Strengthness
+    {
+      get { return getStrengthness(); }
+    }
+
+    private int countMarkTypes()
+    {
+      int i = 0;
+      if (UpperCaseLetters > 0) i++;
+      if (LowerCaseLetters > 0) i++;
+      if (Numbers > 0) i++;
+      if (SpecialMarks > 0) i++;
+      return i;
+    }
+
+    private string getStrengthness()
+    {
+      int markTypes = MarkTypes;
+
+      if (Chars == 0) return "anna salasana";
+
+      //bad = merkkejä vähemmän kuin kahdeksan tai pelkästään yhden merkkiluokan merkkejä
+      if (Chars < 8 || markTypes == 1)
+      {
+        return "bad";
+      }
+
+      //fair = merkkejä vähemmän kuin kaksitoista ja vain kahden merkkiluokan merkkejä
+      if (Chars < 12 || markTypes == 2)
+      {
+        return "fair";
+      }
+
+      //moderate = merkkejä vähemmän kuin kuusitoista ja kolmen merkkiluokan merkkejä
+      if (Chars < 16 || markTypes == 3)
+      {
+        return "moderate";
+      }
+
+      //good = merkkejä vähintään kuusitoista ja sisältää kaikkien neljän merkkiluokan merkkejä
+      if (Chars >= 16 && markTypes == 4)
+      {
+        return "good";
+      }
+
+      return "anna salasana";
+    }
+  }
+}
