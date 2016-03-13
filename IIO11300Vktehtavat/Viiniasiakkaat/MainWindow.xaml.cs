68855b5f14721554168c79926ea096f12ddad27f
Mika M <K6058@student.jamk.fi>
2016-03-13 18:57:08 +0200

    Viikkotehtävä #08, Viiniasiakkaat: Tehtävä aloitettu (UI Bindaukset ei vielä toimi!)

---
+++ IIO11300Vktehtavat/Viiniasiakkaat/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace Viiniasiakkaat
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void btnGetCustomers_Click(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        using (SqlConnection conn = new SqlConnection(Properties.Settings.Default.ConnectionString))
+        {
+          //yhteys
+          //dataadapter
+          const string sql = "SELECT Lastname, Firstname, Address, City FROM vCustomers ORDER BY Lastname";
+          SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+          DataTable dt = new DataTable("Customers");
+          da.Fill(dt);
+          //sidotaan datatable UI-kontrolliin
+          //Aseta ListBoxin dataconteksi DataTable ja aseta sopivasti sen DisplayMemberPath
+          lbxCustomers.DataContext = dt;
+          lbxCustomers.DisplayMemberPath = "Firstname";
+          conn.Close();
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+    }
+  }
+}
