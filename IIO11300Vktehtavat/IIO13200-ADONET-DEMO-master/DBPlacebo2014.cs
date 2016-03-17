e9fe146a3d91f37b272bbb90c77f87584a4f596c
Mika Mähönen <K6058@student.jamk.fi>
2016-03-17 10:33:29 +0200

    Added H10ADOBlanco-project into solution

---
+++ IIO11300Vktehtavat/IIO13200-ADONET-DEMO-master/DBPlacebo2014.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace JAMK.ICT.Data
+{
+  public static class DBPlacebo
+  {
+    public static DataTable GetTestCustomers()
+	{
+    //create
+    DataTable dt = new DataTable();
+    //columns
+    dt.Columns.Add("asioid",typeof(string));
+    dt.Columns.Add("LastName",typeof(string));
+    dt.Columns.Add("FirstName", typeof(string));
+    //rows
+    dt.Rows.Add("A3581", "Waltari","Mika");
+	  dt.Rows.Add("B3553", "King", "Stephen");
+	  dt.Rows.Add("C1238", "Neruda", "Pablo");
+	  dt.Rows.Add("D9876", "Oksanen", "Sofi");
+	return dt;
+	}
+    public static DataTable GetAllCustomersFromSQLServer(string connectionStr, string taulu, string cityFilter, out string viesti)
+    {
+        // basic principle: connect - execute query - disconnect
+        try
+        {
+            SqlConnection myConn = new SqlConnection(connectionStr);
+            myConn.Open();
+            string sql = string.Format("SELECT * FROM {0}", taulu);
+
+            if (!string.IsNullOrEmpty(cityFilter))
+            {
+              sql += string.Format(" WHERE city LIKE '{0}'", cityFilter);
+            }
+
+            SqlCommand cmd = new SqlCommand(sql, myConn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds, taulu);
+            viesti = "Tiedot haettu onnistuneesti tietokannasta " + myConn.DataSource;
+            return ds.Tables[taulu];
+        }
+        catch (Exception ex)
+        {
+            viesti = ex.Message;
+            throw;
+        }
+    }
+
+    public static DataTable GetCitiesFromSQLServer(string connectionStr, string taulu)
+    {
+      try
+      {
+        SqlConnection myConn = new SqlConnection(connectionStr);
+        myConn.Open();
+        SqlCommand cmd = new SqlCommand("SELECT DISTINCT city FROM " + taulu, myConn);
+        SqlDataAdapter da = new SqlDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        da.Fill(ds, taulu);
+        return ds.Tables[taulu];
+      }
+      catch (Exception ex)
+      {
+        throw;
+      }
+    }
+  }
+}
---
+++ IIO11300Vktehtavat/IIO13200-ADONET-DEMO-master/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using JAMK.ICT.Data;
+using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JAMK.ICT.ADOBlanco
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    private string connStr;
+    private string tableName;
+    private DataTable dt;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+      IniMyStuff();
+    }
+
+    private void IniMyStuff()
+    {
+      try
+      {
+        string msg = "";
+        connStr = Properties.Settings.Default.Tietokanta;
+        lbMessages.Content = connStr;
+        tableName = Properties.Settings.Default.Taulu;
+        dt = DBPlacebo.GetAllCustomersFromSQLServer(connStr, tableName, "", out msg);
+        FillMyCombo();
+        lbMessages.Content = msg;
+      }
+      catch (Exception ex)
+      {
+        lbMessages.Content = ex.Message;
+      }
+    }
+
+    private void FillMyCombo()
+    {
+      // Täytetään ComboBox kaupunkien nimillä
+      // VE1 haetaan palvelimelta
+      cbCountries.ItemsSource = DBPlacebo.GetCitiesFromSQLServer(connStr, tableName).DefaultView;
+      cbCountries.DisplayMemberPath = "city";
+      cbCountries.SelectedValuePath = "city";
+
+      // VE2 haetaan dt-muuttujasta
+    }
+
+    private void btnGet3_Click(object sender, RoutedEventArgs e)
+    {
+      dgCustomers.ItemsSource = DBPlacebo.GetTestCustomers().DefaultView;
+    }
+
+    private void btnGetAll_Click(object sender, RoutedEventArgs e)
+    {
+      // Haetaan asiakastietopalvelimelta
+      string msg = "";
+
+      try
+      {
+        // Kaikki asiakkaat DataGridiin
+        dgCustomers.ItemsSource = dt.DefaultView;
+      }
+      catch (Exception ex)
+      {
+        msg = ex.Message;
+      }
+      finally
+      {
+        lbMessages.Content = msg;
+      }
+    }
+
+    private void btnGetFrom_Click(object sender, RoutedEventArgs e)
+    {
+      //TODO
+    }
+
+    private void btnYield_Click(object sender, RoutedEventArgs e)
+    {
+        JSON.JSONPlacebo2015 roskaa = new JSON.JSONPlacebo2015();
+        MessageBox.Show(roskaa.Yield());
+    }
+
+    private void cbCountries_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {      
+      string msg = "";
+
+      try
+      {
+        // Haetaan asiakkaat valitusta kaupungista
+        dt.DefaultView.RowFilter = string.Format("city='{0}'", cbCountries.SelectedValue);
+        dgCustomers.ItemsSource = dt.DefaultView;
+      }
+      catch (Exception ex)
+      {
+        msg = ex.Message;
+      }
+      finally
+      {
+        lbMessages.Content = msg;
+      }
+    }
+
+  }
+}
