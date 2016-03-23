8dff5d6c450a008b71f42c042cc5771855308a7f
Mika M <K6058@student.jamk.fi>
2016-03-23 21:16:33 +0200

    Viikkotehtävä #09, ViiniAsiakkaat Muokkaus  (CRUD): Tehtävä valmis

---
+++ IIO11300Vktehtavat/Viiniasiakkaat/DBCustomer.cs
@@ -0,0 +1,193 @@
+﻿/*
+* Copyright (C) JAMK/IT/Esa Salmikangas
+* This file is part of the IIO11300 course project.
+* Created: 23.3.2016
+* Authors: Mika Mähönen (K6058), Esa Salmikangas
+*/
+
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace JAMK.IT.IIO11300
+{
+  public class DBCustomer
+  {
+    SqlConnection connection;
+    DataTable customers;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public DBCustomer()
+    {
+      connection = new SqlConnection(Viiniasiakkaat.Properties.Settings.Default.ConnectionString);
+    }
+
+    /// <summary>
+    /// Get Customers as DataTable from Database
+    /// </summary>
+    /// <param name="message">return message</param>
+    /// <returns>DataTable containing customers</returns>
+    public DataTable GetCustomers(out string message)
+    {
+      try
+      {
+        const string sql = "SELECT ID, Firstname, Lastname, Address, ZIP, City FROM Customer ORDER BY Lastname, Firstname";
+        SqlDataAdapter da = new SqlDataAdapter(sql, connection);
+        customers = new DataTable("Customers");
+        da.Fill(customers);
+
+        message = string.Format("Haettu {0} asiakkaan tiedot", customers.Rows.Count);
+        return customers;
+      }
+      catch (Exception ex)
+      {
+        message = ex.Message;
+        throw ex;
+      }
+    }
+
+    /// <summary>
+    /// Add Customer to Database
+    /// </summary>
+    /// <param name="customer">Customer to Create</param>
+    /// <param name="message">return message</param>
+    public void CreateCustomer(Customer customer, out string message)
+    {
+      try
+      {
+        const string cmdText = @"
+INSERT INTO customer (Firstname, Lastname, Address, ZIP, City) 
+VALUES (@FirstName, @LastName, @Address, @ZIP, @City)
+";
+
+        SqlCommand command = new SqlCommand(cmdText, connection);
+        command.Parameters.AddWithValue("@FirstName", customer.FirstName);
+        command.Parameters.AddWithValue("@LastName", customer.LastName);
+        command.Parameters.AddWithValue("@Address", customer.Address);
+        command.Parameters.AddWithValue("@ZIP", customer.ZIP);
+        command.Parameters.AddWithValue("@City", customer.City);
+
+        if (connection.State == ConnectionState.Closed)
+        {
+          connection.Open();
+        }
+
+        if (!(command.ExecuteNonQuery() > 0))
+        {
+          throw new Exception("CreateCustomer() failed to save new customer!");
+        }
+
+        message = "Uuden asiakkaan tiedot on tallennettu";
+      }
+      catch (Exception ex)
+      {
+        message = ex.Message;
+        throw ex;
+      }
+      finally
+      {
+        if (connection.State == ConnectionState.Open)
+        {
+          connection.Close();
+        }
+      }
+    }
+
+    /// <summary>
+    /// Update Customer to Database
+    /// </summary>
+    /// <param name="customer">Customer to Update</param>
+    /// <param name="message">return message</param>
+    public void UpdateCustomer(Customer customer, out string message)
+    {
+      try
+      {
+        const string cmdText = @"
+UPDATE customer SET 
+  Firstname = @FirstName,
+  Lastname = @LastName,
+  Address = @Address,
+  ZIP = @ZIP,
+  City = @City
+WHERE
+  ID = @ID
+";
+
+        SqlCommand command = new SqlCommand(cmdText, connection);
+        command.Parameters.AddWithValue("@ID", customer.Id);
+        command.Parameters.AddWithValue("@FirstName", customer.FirstName);
+        command.Parameters.AddWithValue("@LastName", customer.LastName);
+        command.Parameters.AddWithValue("@Address", customer.Address);
+        command.Parameters.AddWithValue("@ZIP", customer.ZIP);
+        command.Parameters.AddWithValue("@City", customer.City);
+
+        if (connection.State == ConnectionState.Closed)
+        {
+          connection.Open();
+        }
+
+        if (!(command.ExecuteNonQuery() > 0))
+        {
+          throw new Exception("UpdateCustomer() failed to update customer details!");
+        }
+
+        message = "Asiakkaan tiedot on päivitetty";
+      }
+      catch (Exception ex)
+      {
+        message = ex.Message;
+        throw ex;
+      }
+      finally
+      {
+        if (connection.State == ConnectionState.Open)
+        {
+          connection.Close();
+        }
+      }
+    }
+
+    /// <summary>
+    /// Delete Customer fromDatabase
+    /// </summary>
+    /// <param name="id">Id of Customer to delete</param>
+    /// <param name="message">return message</param>
+    public void DeleteCustomer(int id, out string message)
+    {
+      try
+      {
+        const string cmdText = @"DELETE FROM customer WHERE ID = @ID";
+
+        SqlCommand command = new SqlCommand(cmdText, connection);
+        command.Parameters.AddWithValue("@ID", id);
+
+        if (connection.State == ConnectionState.Closed)
+        {
+          connection.Open();
+        }
+
+        if (!(command.ExecuteNonQuery() > 0))
+        {
+          throw new Exception("DeleteCustomer() failed to delete customer!");
+        }
+
+        message = "Asiakkaan tiedot on poistettu";
+      }
+      catch (Exception ex)
+      {
+        message = ex.Message;
+        throw ex;
+      }
+      finally
+      {
+        if (connection.State == ConnectionState.Open)
+        {
+          connection.Close();
+        }
+      }
+    }
+
+  }
+}
---
+++ IIO11300Vktehtavat/Viiniasiakkaat/MainWindowCRUD.xaml.cs
@@ -0,0 +1,180 @@
+﻿using JAMK.IT.IIO11300;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Viiniasiakkaat
+{
+  /// <summary>
+  /// Interaction logic for MainWindowCRUD.xaml
+  /// </summary>
+  public partial class MainWindowCRUD : Window
+  {
+    DBCustomer dao;
+    
+
+    public MainWindowCRUD()
+    {
+      InitializeComponent();
+      IniMyStaff();
+    }
+
+    private void IniMyStaff()
+    {
+      dao = new DBCustomer();
+    }
+
+    private void btnGetCustomers_Click(object sender, RoutedEventArgs e)
+    {
+      string message = "";
+
+      try
+      {
+        grdCustomers.DataContext = dao.GetCustomers(out message);
+      }
+      catch (Exception ex)
+      {
+        message = ex.Message;
+        MessageBox.Show(ex.Message);
+      }
+      finally
+      {
+        lblMessage.Text = message;
+      }
+      
+    }
+
+    private void btnNewCustomer_Click(object sender, RoutedEventArgs e)
+    {
+      spNewCustomer.Height = 20;
+      txtFirstName.Focus();
+    }
+
+    private void btnCreateCustomer_Click(object sender, RoutedEventArgs e)
+    {
+      if (HasDetailsErrors()) return;
+
+      string message = "";
+
+      try
+      {
+        Customer customer = new Customer();
+        customer.FirstName = txtFirstName.Text;
+        customer.LastName = txtLastName.Text;
+        customer.Address = txtAddress.Text;
+        customer.ZIP = txtZIP.Text;
+        customer.City = txtCity.Text;
+        dao.CreateCustomer(customer, out message);
+        MessageBox.Show(message);
+
+        btnGetCustomers_Click(this, null);
+        spNewCustomer.Height = 0;
+      }
+      catch (Exception ex)
+      {
+        message = ex.Message;
+        MessageBox.Show(ex.Message);
+      }
+      finally
+      {
+        lblMessage.Text = message;
+      }
+    }
+
+    private bool HasDetailsErrors()
+    {
+      bool errors = false;
+      string message = "";
+      TextBox txt = null;
+
+      if (string.IsNullOrWhiteSpace(txtFirstName.Text))
+      {
+        txt = txtFirstName;
+        message = "etunimi puuttuu";
+        errors = true;
+      }
+      else if (string.IsNullOrWhiteSpace(txtLastName.Text))
+      {
+        txt = txtLastName;
+        message = "sukunimi puuttuu";
+        errors = true;
+      }
+      else if (string.IsNullOrWhiteSpace(txtAddress.Text))
+      {
+        txt = txtAddress;
+        message = "osoite puuttuu";
+        errors = true;
+      }
+      else if (string.IsNullOrWhiteSpace(txtZIP.Text))
+      {
+        txt = txtZIP;
+        message = "postinumero puuttuu";
+        errors = true;
+      }
+      else if (string.IsNullOrWhiteSpace(txtCity.Text))
+      {
+        txt = txtCity;
+        message = "kaupunki puuttuu";
+        errors = true;
+      }
+
+      if (errors)
+      {
+        MessageBox.Show(string.Format("Ei voi tallentaa koska {0}!", message));
+        txt.Focus();
+      }
+
+      return errors;
+    }
+
+    private void btnDeleteCustomer_Click(object sender, RoutedEventArgs e)
+    {
+      string message = "";
+
+      try
+      {
+        if (grdCustomers.SelectedItem == null)
+        {
+          MessageBox.Show("Valitse ensin asiakas!");
+          return;
+        }
+
+        object[] items = ((DataRowView)grdCustomers.SelectedItem).Row.ItemArray;
+        int id = (int)items[0];
+        string firstName = (string)items[1];
+        string lastName = (string)items[2];
+
+        MessageBoxResult result = MessageBox.Show(
+            string.Format("Haluatko varmasti poistaa asiakkaan {0} {1}?", firstName, lastName),
+            "Viinikellarin asiakkaat",
+            MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+
+        if (result == MessageBoxResult.Yes)
+        {
+          dao.DeleteCustomer(id, out message);
+          message = string.Format("Asiakas {0} {1} on poistettu", firstName, lastName);
+          btnGetCustomers_Click(this, null);
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+      finally
+      {
+        lblMessage.Text = message;
+      }
+    }
+  }
+}
